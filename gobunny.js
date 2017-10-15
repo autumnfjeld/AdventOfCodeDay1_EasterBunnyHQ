3e79a1de28cfbb2be2d7019b96b24d4b08fd6ac1
Autumn Fjeld <autumn@talentbin.com>
2017-10-15 22:30:13 +1100

    Add run function with provided input and set up as npm script command

--- gobunny.js
+++ gobunny.js
@@ -130,9 +130,33 @@ FindEasterBunnyHQ.prototype.hopAlongTheBlocks = function () {
  * Compute the minimum number of blocks Dr. Bunny could travel to get from her start position to final position
  * Start position is [0,0]
  */
-FindEasterBunnyHQ.prototype.computeMinBunnyBlocks = function(){
+FindEasterBunnyHQ.prototype.computeMinBunnyBlocks = function () {
     this.minimumBlocksAway = Math.abs(this.position.x - 0) + Math.abs(this.position.y - 0);
 };
 
 
-module.exports = FindEasterBunnyHQ;
+/****************************************************************************
+ *  Run Easter Bunny HQ App with input data
+ ****************************************************************************/
+
+/**
+ * Export function to call via npm script tor run app with specified input data
+ * @returns {null|*}
+ */
+function run() {
+    // Provided input from my http://adventofcode.com/2016/day/1/input
+    var input = {
+        sequence: ['L2', 'L3', 'L3', 'L4', 'R1', 'R2', 'L3', 'R3', 'R3', 'L1', 'L3', 'R2', 'R3', 'L3', 'R4', 'R3', 'R3', 'L1', 'L4', 'R4', 'L2', 'R5', 'R1', 'L5', 'R1', 'R3', 'L5', 'R2', 'L2', 'R2', 'R1', 'L1', 'L3', 'L3', 'R4', 'R5', 'R4', 'L1', 'L189', 'L2', 'R2', 'L5', 'R5', 'R45', 'L3', 'R4', 'R77', 'L1', 'R1', 'R194', 'R2', 'L5', 'L3', 'L2', 'L1', 'R5', 'L3', 'L3', 'L5', 'L5', 'L5', 'R2', 'L1', 'L2', 'L3', 'R2', 'R5', 'R4', 'L2', 'R3', 'R5', 'L2', 'L2', 'R3', 'L3', 'L2', 'L1', 'L3', 'R5', 'R4', 'R3', 'R2', 'L1', 'R2', 'L5', 'R4', 'L5', 'L4', 'R4', 'L2', 'R5', 'L3', 'L2', 'R4', 'L1', 'L2', 'R2', 'R3', 'L2', 'L5', 'R1', 'R1', 'R3', 'R4', 'R1', 'R2', 'R4', 'R5', 'L3', 'L5', 'L3', 'L3', 'R5', 'R4', 'R1', 'L3', 'R1', 'L3', 'R3', 'R3', 'R3', 'L1', 'R3', 'R4', 'L5', 'L3', 'L1', 'L5', 'L4', 'R4', 'R1', 'L4', 'R3', 'R3', 'R5', 'R4', 'R3', 'R3', 'L1', 'L2', 'R1', 'L4', 'L4', 'L3', 'L4', 'L3', 'L5', 'R2', 'R4', 'L2']
+    };
+
+    var bunnyTrip = new FindEasterBunnyHQ(input.sequence);
+    return bunnyTrip.minimumBlocksAway;
+}
+
+
+module.exports = {
+    FindEasterBunnyHQ: FindEasterBunnyHQ,
+    run: run
+};
+
+require('make-runnable');
